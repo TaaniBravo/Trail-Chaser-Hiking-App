a1a9ef01ae0bda6e252e62277eb6c6f4ffdefd36
smedigo <smedigo@gmail.com>
2020-11-14 11:10:02 -0800

    startedweatherfunction

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+
+
+
+function handleweatherinformation() {
+
+    var APIKey = "52aa85fe9180c06fe869a1a3e7d7de19"
+    var lat = 45.7581747
+
+    var long = -121.5425736
+var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&appid="+ APIKey;
+
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+
+     
+    console.log (queryURL)
+
+    
+    console.log(response)
+    appendweatherinfo(response)
+  })}
+
+function appendweatherinfo(response) {
+
+    
+
+
+}
