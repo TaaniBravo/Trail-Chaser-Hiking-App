963df3f5814605cdac2d7b786f22cea4f3edcdcc
Taani Maama <taani1maama@live.com>
2020-11-15 21:13:46 -0800

    Added some functions to seperate and organize what each of them should be doing so there are no extra tasks a function is going to do.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,9 @@
-$(document).ready(handleweatherinformation)
+// DECLARE VARIABLES
+let directionsRenderer;
+let directionsService;
+let map;
+
+$(document).ready(handleWeatherInfo)
 
 // This button events apply to the modal and what happens when each of the buttons are clicked.
 $('#closeBtn').on('click', handleGeoLocation)
@@ -12,8 +17,8 @@ function initMap() {
   let lat = 47.5518333
   let long = -122.82669
 
-  // LET these variables provide the directions and the display them.
-  let directionsDisplay = new google.maps.DirectionsRenderer;
+  let directionsService = new google.maps.DirectionsService;
+  let directionsRenderer = new google.maps.DirectionsRenderer;
 
   // The location of hikeLocation
   const hikeLocation = { lat: lat, lng: long };
@@ -22,13 +27,14 @@ function initMap() {
     zoom: 8,
     center: hikeLocation,
   });
+  directionsRenderer.setMap(map)
   // The marker, positioned at hikeLocation
   const marker = new google.maps.Marker({
     position: hikeLocation,
     map: map,
   });
 
-  directionsDisplay.setMap(map)
+ 
 }
 
 function handleGeoLocation() {
@@ -36,19 +42,20 @@ function handleGeoLocation() {
   $('.modal').removeClass('is-active')
   // THEN we pull the geo location
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(handleMap)
+    navigator.geolocation.getCurrentPosition(handleUserAddress)
   }
   else {
     return;
   }
 }
 
-function handleMap(position, destination) {
+function handleUserAddress(position) {
   const googleAPI = 'AIzaSyBhOGyxS_RiEneLIpqf6mUUIL2HI2sEms4'
+
   // LET these variables be equal to the user's latitude and longitude
   let userLatitude = position.coords.latitude
   let userLongitude = position.coords.longitude
-  let directionsService = new google.maps.DirectionsService;
+
   // First we need to turn the geolocation of the user into a valid address for Google to use.
   const reverseGeoURL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${userLatitude},${userLongitude}&key=${googleAPI}`
 
@@ -56,34 +63,34 @@ function handleMap(position, destination) {
     url: reverseGeoURL,
     method: 'GET'
   }).then(function (response) {
-    let placeID = response.results[8].place_id
-    let lat = 47.5518333
-    let long = -122.82669
-    let destination = new google.maps.LatLng(lat, long);
-
-    // const directionsURL = `https://maps.googleapis.com/maps/api/directions/json?origin=${placeID}&destination=${lat},${long}&key=${googleAPI}`
-
-    // $.ajax({
-    //   url: directionsURL,
-    //   method: 'GET'
-    // }).then(function (response) {
-    //   console.log(response)
-    // })
-    let request = {
-      origin: placeID,
-      destination: destination,
-      travelMode: "DRIVING"
-    }
-    directionsService.route(request, function (response, status) {
-      if (status === 'OK') {
-        directionsDisplay.setDirections(response);
-      }
-    })
+    // console.log(response)
+    let userAddress = response.results[0].formatted_address
+    calcRoute(userAddress)
   })
-
 }
 
-function handleweatherinformation() {
+function calcRoute(userAddress) {
+
+  let destination = new google.maps.LatLng(47.5518333, -122.82669);
+  let directionsService = new google.maps.DirectionsService
+  let directionsRenderer = new google.maps.DirectionsRenderer
+
+  let request = {
+    origin: userAddress,
+    destination: destination,
+    travelMode: "DRIVING"
+  }
+
+  console.log(request)
+  directionsService.route(request, function(result, status) {
+    console.log(result)
+    if (status == 'OK') {
+      directionsRenderer.setDirections(result);
+    }
+  });
+}
+
+function handleWeatherInfo() {
 
   var APIKey = "52aa85fe9180c06fe869a1a3e7d7de19"
   var lat = 45.7581747
@@ -98,11 +105,6 @@ function handleweatherinformation() {
     method: "GET"
   }).then(function (response) {
 
-
-    console.log(queryURL)
-
-
-    console.log(response)
     appendweatherinfo(response)
   })
 }
@@ -115,10 +117,6 @@ function appendweatherinfo(response) {
     var tempF = (response.daily[i].temp.day - 273.15) * 1.80 + 32
 
     $("<p>").text(tempF.toFixed(2) + "°F").appendTo(weatherAppend);
-
-
-
-    console.log((response.daily[i].temp.day - 273.15) * 1.80 + 32);
 
   }
 
