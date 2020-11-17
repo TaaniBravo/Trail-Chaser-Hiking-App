b2cdf8e5037c096bb641118e202acd0fa35ddc3f
Bryan Moreno <bfourbryan@gmail.com>
2020-11-16 16:31:50 -0800

    bm- Working if statement that logs the user inputs to local storage

--- assets/indexscript.js
+++ assets/indexscript.js
@@ -43,87 +43,133 @@ function handleUserInfo() {
         //var starInput = $("#ratingInput").val();
 
         var userData = [locationInput, radiusInput, lengthInput, dateInput, difficultyInput, starInput]
-            console.log(userData);
-        //localStorage.setItem("location", locationInput);
-        //localStorage.setItem("radius", radiusInput);
-        //localStorage.setItem("length", lengthInput);
-        //localStorage.setItem("date", dateInput);
-        //localStorage.setItem("difficultyInput", difficultyInput);
-        //localStorage.setItem("ratingInput", starInput);
 
-        //function persistInput(input) {
+        //localStorage.setItem("flag", "set");
+
+        //var userData = [
+            //{
+                //name: location,
+                //value: locationInput,
+            //},
+            //{
+                //name: radius,
+                //value: radiusInput,
+            //},
+            //{
+                //name: length,
+                //value: lengthInput,
+            //},
+            //{
+                //name: date,
+                //value: dateInput,
+            //},
+            //{
+                //name: difficultyInput,
+                //value: difficultyInput,
+            //},
+            //{
+                //name: ratingInput,
+                //value: starInput,
+            //}
+        //];
+
+        //$.each(userData, function (i, index) {
+            //localStorage.setItem(obj.name, obj.value);
+        //});
+        
+
+        //if (localStorage.getItem("flag") == "set") {
+            //$("header").after("<p>This form has saved data!</p>");
+
+            //$.each(userData, function (i, obj) {
+                //$("[name= '" + obj.name + "']").val(localStorage.getItem(obj.name, obj.value));
+
+            //});
+
+
+
+
+            localStorage.setItem("location", locationInput);
+            localStorage.setItem("radius", radiusInput);
+            localStorage.setItem("length", lengthInput);
+            localStorage.setItem("date", dateInput);
+            localStorage.setItem("difficultyInput", difficultyInput);
+            localStorage.setItem("ratingInput", starInput);
+            console.log(userData);
+
+            //function persistInput(input) {
             //var key = "input-" + input.id;
 
             //var storedValue = localStorage.getItem(key);
 
             //if (storedValue)
-                //input.value = storedValue;
+            //input.value = storedValue;
 
             //input.addEventListener('input', function () {
-                //localStorage.setItem(key, input.value);
+            //localStorage.setItem(key, input.value);
             //});
-        //}
-        //function handleStorage() {
-            
+            //}
+            //function handleStorage() {
+
             //e.preventDefault();
 
             //var userData = document.getElementById('form-group').serializeArray();
             //console.log(userData);
-        //};
+            //};
 
-        console.log(taylorSwift);
+            console.log(taylorSwift);
 
 
 
-    }
-    else {
-        console.log(checkSaveCriteria);
+        }
+        else {
+            console.log(checkSaveCriteria);
+        };
+
+
     };
 
-
-}
-
-// handleSearch - make ajax call and get response info for hikes to appear
-// TO DO - update the query URL with input from handleUserInfo
-function handleSearch() {
-    queryURL = `https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=${apiID}`;
-    // Perfoming an AJAX GET request to our queryURL
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        // After the data from the AJAX request comes back
-        .then(function (response) {
-            // console.log(response);
-            handleResults(response);
-        });
-}
-
-// handleResults - display results of first 5 results in card form
-function handleResults(response) {
-    // console.log(response);
-    $("#results").empty();
-    for (var i = 0; i < 5; i++) {
-        // console.log(response.trails[i]);
-        $("#results").append(response.trails[i].name);
-        $("#results").append("<br>");
-        console.log(response.trails[i].name);
-        console.log(response.trails[i].imgSqSmall);
-        console.log(response.trails[i].stars);
+    // handleSearch - make ajax call and get response info for hikes to appear
+    // TO DO - update the query URL with input from handleUserInfo
+    function handleSearch() {
+        queryURL = `https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=${apiID}`;
+        // Perfoming an AJAX GET request to our queryURL
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            // After the data from the AJAX request comes back
+            .then(function (response) {
+                // console.log(response);
+                handleResults(response);
+            });
     }
 
-}
+    // handleResults - display results of first 5 results in card form
+    function handleResults(response) {
+        // console.log(response);
+        $("#results").empty();
+        for (var i = 0; i < 5; i++) {
+            // console.log(response.trails[i]);
+            $("#results").append(response.trails[i].name);
+            $("#results").append("<br>");
+            console.log(response.trails[i].name);
+            console.log(response.trails[i].imgSqSmall);
+            console.log(response.trails[i].stars);
+        }
 
-// TO DO - create listener for when user clicks on search result
+    }
 
-// TO DO - create function to save search criteria if user clicks checkbox
+    // TO DO - create listener for when user clicks on search result
 
-
-
-//if ("box is checked") {
-//store my criteria checked
-//}
-// function handleStorage() {}
+    // TO DO - create function to save search criteria if user clicks checkbox
 
 
-$("#findBtn").on("click", handleUserInfo);
+
+    //if ("box is checked") {
+    //store my criteria checked
+    //}
+    // function handleStorage() {}
+
+
+    $("#findBtn").on("click", handleUserInfo);
