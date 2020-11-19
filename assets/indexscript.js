402abf9a88045a2eab5d925099f59e40f6fae2d0
Sally Perez <seattlesally1@gmail.com>
2020-11-18 16:40:36 -0800

    sp - added catch for zero resuls and displays alert, updated css to change length of results cards with a hover color

--- assets/indexscript.js
+++ assets/indexscript.js
@@ -4,7 +4,7 @@ var apiID = "200970639-981a2550ac3c48f2579397ecf3a9b65e";
 var queryURL;
 var resultsEl = $("#results");
 var hikesReturned;
-var userHikeSelected; 
+var userHikeSelected;
 var locationInput;
 var radiusInput;
 var lengthInput;
@@ -17,136 +17,124 @@ var lon;
 var savedCriteria = JSON.parse(localStorage.getItem("savedCriteria")) || [];
 
 function init() {
-    if (savedCriteria != []) {
-            console.log("stuff was saved bro!");
-            $("#location").val(savedCriteria.location);
-            $("#length").val(savedCriteria.length);
-            $("#radius").val(savedCriteria.radius);
-            $("#date").val(savedCriteria.date);
-            $("#difficultyInput").val(savedCriteria.difficultyInput);
-            $("#ratingInput").val(savedCriteria.ratingInput);
-        };
-};
+  if (savedCriteria != []) {
+    console.log("stuff was saved bro!");
+    $("#location").val(savedCriteria.location);
+    $("#length").val(savedCriteria.length);
+    $("#radius").val(savedCriteria.radius);
+    $("#date").val(savedCriteria.date);
+    $("#difficultyInput").val(savedCriteria.difficultyInput);
+    $("#ratingInput").val(savedCriteria.ratingInput);
+  }
+}
 // handleUserInfo - get user inputs
 function handleUserInfo() {
-    // if (savedCriteria != []) {
-    //     console.log("stuff was saved bro!");
-    //     $("#location").val("Chicadgo")
-    //     $("#length").val(3);
-    // };
-    
-    
-    
-    
-    
-    
-    
-    // get inputs
-    locationInput = $("#location").val();
-    // TO DO - do some checks to make sure a place was already exits
-    radiusInput = $("#radius").val();
-    // TO DO - check radius and length are actual numbers
-    lengthInput = $("#length").val();
-    dateInput = $("#date").val();
-    difficultyInput = $("#difficultyInput").val();
-    starInput = $("#ratingInput").val();
+  // if (savedCriteria != []) {
+  //     console.log("stuff was saved bro!");
+  //     $("#location").val("Chicadgo")
+  //     $("#length").val(3);
+  // };
 
-    let checkSaveCriteria = document.getElementById('checkboxChecker').checked;
+  // get inputs
+  locationInput = $("#location").val();
+  // TO DO - do some checks to make sure a place was already exits
+  radiusInput = $("#radius").val();
+  // TO DO - check radius and length are actual numbers
+  lengthInput = $("#length").val();
+  dateInput = $("#date").val();
+  difficultyInput = $("#difficultyInput").val();
+  starInput = $("#ratingInput").val();
 
-    // I Created this variable just to see if my if statement was working.
-    var taylorSwift = "We're all Taylor Swift fans here!";
+  let checkSaveCriteria = document.getElementById("checkboxChecker").checked;
 
-    console.log(locationInput);
-    console.log(radiusInput);
-    console.log(lengthInput);
-    console.log(dateInput);
-    console.log(difficultyInput);
-    console.log(starInput);
+  // I Created this variable just to see if my if statement was working.
+  var taylorSwift = "We're all Taylor Swift fans here!";
 
-    console.log(checkSaveCriteria);
+  console.log(locationInput);
+  console.log(radiusInput);
+  console.log(lengthInput);
+  console.log(dateInput);
+  console.log(difficultyInput);
+  console.log(starInput);
 
-    // make ajax call
-    handleCity();
-    // handleSearch();
+  console.log(checkSaveCriteria);
 
-    // display results
-    if (checkSaveCriteria === true) {
-        //var locationInput = $("#location").val();
-        //var radiusInput = $("#radius").val();
-        //var lengthInput = $("#length").val();
-        //var dateInput = $("#date").val();
-        //var difficultyInput = $("#difficultyInput").val();
-        //var starInput = $("#ratingInput").val();
+  // make ajax call
+  handleCity();
+  // handleSearch();
 
-        var userData = {
-            location: locationInput, 
-            radius: radiusInput, 
-            length: lengthInput, 
-            date: dateInput, 
-            difficultyInput: difficultyInput, 
-            ratingInput: starInput
-        };
+  // display results
+  if (checkSaveCriteria === true) {
+    //var locationInput = $("#location").val();
+    //var radiusInput = $("#radius").val();
+    //var lengthInput = $("#length").val();
+    //var dateInput = $("#date").val();
+    //var difficultyInput = $("#difficultyInput").val();
+    //var starInput = $("#ratingInput").val();
 
-        //savedCriteria.push(userData)
-
-        //$.each(userData, function (i, index) {
-        //localStorage.setItem(key, value);
-        //});
-
-        localStorage.setItem("savedCriteria", JSON.stringify(userData));
-        //function that appends info into form
-        
-        
-        //function handleStorage() {
-        //var savedUserData = localStorage.getItem("location");
-        //document.getElementById("location").innerText = savedUserData;
-        //};
-        //$.each(userData, function (i, index) {
-            //localStorage.getItem(index.key, index.value);
-            //document.getElementById().innerHTML
-        //});
-        //e.preventDefault();
-
-        console.log(taylorSwift);
-
-    }
-    else {
-        localStorage.clear("savedCriteria");
+    var userData = {
+      location: locationInput,
+      radius: radiusInput,
+      length: lengthInput,
+      date: dateInput,
+      difficultyInput: difficultyInput,
+      ratingInput: starInput,
     };
 
+    //savedCriteria.push(userData)
 
-};
+    //$.each(userData, function (i, index) {
+    //localStorage.setItem(key, value);
+    //});
+
+    localStorage.setItem("savedCriteria", JSON.stringify(userData));
+    //function that appends info into form
+
+    //function handleStorage() {
+    //var savedUserData = localStorage.getItem("location");
+    //document.getElementById("location").innerText = savedUserData;
+    //};
+    //$.each(userData, function (i, index) {
+    //localStorage.getItem(index.key, index.value);
+    //document.getElementById().innerHTML
+    //});
+    //e.preventDefault();
+
+    console.log(taylorSwift);
+  } else {
+    localStorage.clear("savedCriteria");
+  }
+}
 
 // handleSearch - make ajax call and get response info for hikes to appear
 // TO DO - update the query URL with input from handleUserInfo
 function handleSearch() {
+  // TO DO - there is no difficulty input parameter
 
-    // TO DO - there is no difficulty input parameter
-
-    queryURL = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=${radiusInput}&minLength=${lengthInput}&minStars=${starInput}&key=${apiID}`;
-    console.log(queryURL);
-    // Perfoming an AJAX GET request to our queryURL
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        // After the data from the AJAX request comes back
-        .then(function (response) {
-            console.log(response);
-            handleResults(response);
-        });
+  queryURL = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=${radiusInput}&minLength=${lengthInput}&minStars=${starInput}&key=${apiID}`;
+  console.log(queryURL);
+  // Perfoming an AJAX GET request to our queryURL
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  })
+    // After the data from the AJAX request comes back
+    .then(function (response) {
+      console.log(response);
+      handleResults(response);
+    });
 }
 
 // handleResults - display results of first 5 results in card form
 function handleResults(response) {
-    // console.log(response);
-    hikesReturned = response.trails; // store for use when user clicks selection
-    // console.log(hikesReturned)
-    resultsEl.empty(); // clearresults section
-    for (var i = 0; i < 5; i++) {
-        // console.log(response.trails[i]);
-        // create a card with info
-        var card = `<div class="card" id="${i}">
+  // console.log(response);
+  hikesReturned = response.trails; // store for use when user clicks selection
+  // console.log(hikesReturned)
+  resultsEl.empty(); // clearresults section
+  for (var i = 0; i < 5; i++) {
+    // console.log(response.trails[i]);
+    // create a card with info
+    var card = `<div class="card" id="${i}">
                         <div class="card-content">
                             <div class="media">
                                 <div class="media-left">
@@ -161,41 +149,44 @@ function handleResults(response) {
                             </div> 
                         </div> 
                     </div>`;
-        resultsEl.append(card);
-    }
-
+    resultsEl.append(card);
+  }
 }
 
-
 function handleCity() {
-    const googleAPI = 'AIzaSyBhOGyxS_RiEneLIpqf6mUUIL2HI2sEms4'
-  
-    // First we need to turn the geolocation of the user into a valid address for Google to use.
-    const geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationInput}&key=${googleAPI}`
-  
-    $.ajax({
-      url: geocodeURL,
-      method: 'GET'
-    }).then(function (response) {
+    const googleAPI = "AIzaSyBhOGyxS_RiEneLIpqf6mUUIL2HI2sEms4";
+    
+  // First we need to turn the geolocation of the user into a valid address for Google to use.
+  const geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationInput}&key=${googleAPI}`;
+
+  $.ajax({
+    url: geocodeURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    if (response.status == "OK") {
       lat = response.results[0].geometry.location.lat;
       lon = response.results[0].geometry.location.lng;
-      console.log(lat, lon)
+      console.log(lat, lon);
       handleSearch();
-    }) // catch a 404!
-  }
+    } else if (response.status == "ZERO_RESULTS") {
+      alert("Error! City not found!"); // replace with modal?
+      $("#results").empty();
+    }
+  });
+}
 
 // TO DO - create listener for when user clicks on search result
-$("#results").on("click", ".card", function() {
-    // console.log("you clicked a hike!" + $(this).attr('id'));
-    userHikeSelected = hikesReturned[$(this).attr('id')];
-    // use userHikeSelected in script.js
-    // console.log(userHikeSelected);
-    localStorage.setItem("hikeSelected", JSON.stringify(userHikeSelected));
-    window.location.href = "results.html";
+$("#results").on("click", ".card", function () {
+  // console.log("you clicked a hike!" + $(this).attr('id'));
+  userHikeSelected = hikesReturned[$(this).attr("id")];
+  // use userHikeSelected in script.js
+  // console.log(userHikeSelected);
+  localStorage.setItem("hikeSelected", JSON.stringify(userHikeSelected));
+  window.location.href = "results.html";
 });
 
 // TO DO - create function to save search criteria if user clicks checkbox
 
 init();
 $("#findBtn").on("click", handleUserInfo);
-
